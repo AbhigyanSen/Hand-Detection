c7d3de0c96ceb78f30939d4dc21e7b32fb9c536e
Abhigyan Sen <62685639+AbhigyanSen@users.noreply.github.com>
2023-11-20 11:56:43 +0530

    Displaying FPS

--- Hand Detection/HandDetection.py
+++ Hand Detection/HandDetection.py
@@ -9,6 +9,10 @@ hands = mpHands.Hands()
 # Drawing a Line Beyween Each Detected Point
 mpDraw = mp.solutions.drawing_utils
 
+# Frame Rate
+PrevTime = 0
+CurrTime = 0
+
 while True:
     success, img = cap.read()
     ImageRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -18,6 +22,12 @@ while True:
     if Results.multi_hand_landmarks:
         for HandLandmarks in Results.multi_hand_landmarks:
             mpDraw.draw_landmarks(img, HandLandmarks, mpHands.HAND_CONNECTIONS)
+    
+    # FPS (Frame Rate)
+    CurrTime = time.time()
+    FPS = 1/(CurrTime - PrevTime)
+    PrevTime = CurrTime
+    cv2.putText(img, str(int(FPS)), (10,70), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 250, 255), 2)
     
     cv2.imshow("Image", img)
     cv2.waitKey(1)
