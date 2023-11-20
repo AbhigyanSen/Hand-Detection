ddd62d8134f27d3a5699eddf4e483798284d1e3c
Abhigyan Sen <62685639+AbhigyanSen@users.noreply.github.com>
2023-11-20 11:50:51 +0530

    Detecting Hands from Feed
    The Landmarks for the Hand Detections are being Displayed in the Live Feed

--- Hand Detection/HandDetection.py
+++ Hand Detection/HandDetection.py
@@ -6,11 +6,18 @@ cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 
+# Drawing a Line Beyween Each Detected Point
+mpDraw = mp.solutions.drawing_utils
+
 while True:
     success, img = cap.read()
     ImageRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     Results = hands.process(ImageRGB)
     print(Results.multi_hand_landmarks)
     
-    # cv2.imshow("Image", img)
-    # cv2.waitKey(1)
+    if Results.multi_hand_landmarks:
+        for HandLandmarks in Results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, HandLandmarks, mpHands.HAND_CONNECTIONS)
+    
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
