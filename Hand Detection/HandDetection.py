e01e3195a80101bb7be6375ac3e9990b65b4d657
Abhigyan Sen <62685639+AbhigyanSen@users.noreply.github.com>
2023-11-20 11:42:57 +0530

    Detecting Hands from Feed
    On Hands Detection, the co-ordinates of the hands were shown in the terminal

--- Hand Detection/HandDetection.py
+++ Hand Detection/HandDetection.py
@@ -1,11 +1,16 @@
 import cv2
-import mediapipe
+import mediapipe as mp
 import time
 
 cap = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
 
 while True:
     success, img = cap.read()
+    ImageRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    Results = hands.process(ImageRGB)
+    print(Results.multi_hand_landmarks)
     
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    # cv2.imshow("Image", img)
+    # cv2.waitKey(1)
